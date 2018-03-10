07a30900afa0065ce71989fa2b9c861372864753
igoryan.ru@mail.ru <QaZwSxEdC123123>
2018-03-10 21:07:13 +0500

    Домашка1

--- app/src/main/java/com/example/igory/learn1/MainActivity.java
+++ app/src/main/java/com/example/igory/learn1/MainActivity.java
@@ -1,13 +1,108 @@
 package com.example.igory.learn1;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener{
+
+    Button myBttn;
+
+    private static final String TAG = "Main";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.myscreen);
+
+        setContentView(R.layout.activity_main);
+
+        myBttn = (Button) findViewById(R.id.button);
+
+        myBttn.setOnClickListener(this);
+
+        Log.d(TAG, "onCreate");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent toSecondActivity = new Intent(this, SecondActivity.class);
+        startActivity(toSecondActivity);
+    }
+
+    public void onCheckboxClicked(View view) {
+        // Получаем флажок
+        CheckBox language = (CheckBox) view;
+        // Получаем, отмечен ли данный флажок
+        boolean checked = language.isChecked();
+
+        if (checked)
+        {
+            if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT)
+            {
+                Log.d(TAG, "2222222222222");
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+            else //if (getRequestedOrientation() == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+            {
+                Log.d(TAG, "33333333333333");
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            }
+        }
+        else
+        {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+        }
     }
 }
